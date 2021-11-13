d3fe4c8d0ada54d47b7a8c714541bd0d7f04a580
denbich <denis.bichler9@gmail.com>
2021-11-13 12:46:15 +0100

    Update SetLocale.php

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -13,11 +13,11 @@ class SetLocale
     public function handle(Request $request, Closure $next)
     {
         if (session('locale') != null) {
-            App::setLocale('pl');
-            //App::setLocale(Session::get('locale'));
+            //App::setLocale('pl');
+            App::setLocale(Session::get('locale'));
         } else if (Auth::check()) {
-            App::setLocale('pl');
-            //App::setLocale(strtolower(Auth::user()->country));
+            //App::setLocale('pl');
+            App::setLocale(strtolower(Auth::user()->country));
         } else {
             App::setLocale('pl');
         }
